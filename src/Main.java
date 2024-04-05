3949070f931018e41b4a23e64237a4a59a6e73b1
bin <kiia8858@naver.com>
2024-04-06 00:51:21 +0900

    4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.OptionalDouble;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void NO1(String[] args){
@@ -18,12 +19,30 @@ public class Main {
         System.out.println(aa);
     }
 
-    public static void main(String[] args){
+    public static void NO3(String[] args){
         //num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수
         int a =3 ;
         int b =4 ;
         String bbb = (a%2 == 0) ? "Even" : "Odd";
         System.out.println(bbb);
+    }
+
+    public static void main(String[] args){
+        // N = 123이면 1 + 2 + 3 = 6을
+        int a =123 ;
+        int b =4556 ;
+        String stringb = String.valueOf(a); // 문자열 바꾸고
+        String[] split=stringb.split(""); //문자열을 분리해서
+        IntStream intStream = Arrays.stream(split).mapToInt(x -> Integer.parseInt(x));
+        int result=intStream.sum();
+        System.out.println(result);
+//        System.out.println(split[3]);
+
+
+
+
+
+
     }
 
 
