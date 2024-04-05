9b8699ff1cfe142ce0fb690916029cd3c017e5c2
bin <kiia8858@naver.com>
2024-04-05 23:54:36 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public static void NO1(String[] args){
+        //정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+        int[] arr = {1,2,3,4};
+        IntStream intStream = Arrays.stream(arr);
+        OptionalDouble OD = intStream.average();
+        Double aa =OD.getAsDouble();
     }
 }
