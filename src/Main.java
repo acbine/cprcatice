7d78b0da5b1bf14fddb8ddf575cc03cccc8236ce
bin <kiia8858@naver.com>
2024-04-06 00:12:40 +0900

    2

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-
     public static void NO1(String[] args){
         //정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
         int[] arr = {1,2,3,4};
@@ -14,4 +13,15 @@ public class Main {
         OptionalDouble OD = intStream.average();
         Double aa =OD.getAsDouble();
     }
+    public static void NO2(String[] args){
+        //str이 "1234"이면 1234를 반환하고, "-1234"이면 -1234를 반환하면
+        String str1= "1234";
+        String str2= "-1234";
+        int aa = Integer.parseInt(str2);
+        System.out.println(aa);
+    }
+
+
+
+
 }
