e14658d0a76e4f46ae2fd78f7207e3323719299b
bin <kiia8858@naver.com>
2024-04-06 00:23:22 +0900

    3

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
     public static void NO1(String[] args){
         //정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
         int[] arr = {1,2,3,4};
@@ -19,6 +16,14 @@ public class Main {
         String str2= "-1234";
         int aa = Integer.parseInt(str2);
         System.out.println(aa);
+    }
+
+    public static void main(String[] args){
+        //num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수
+        int a =3 ;
+        int b =4 ;
+        String bbb = (a%2 == 0) ? "Even" : "Odd";
+        System.out.println(bbb);
     }
 
 
