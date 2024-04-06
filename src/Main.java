94e8cc485cb8830d49041f52b2d234912381f73b
bin <kiia8858@naver.com>
2024-04-07 00:16:39 +0900

    7

--- src/Main.java
+++ src/Main.java
@@ -66,6 +66,18 @@ public class Main {
         System.out.println(array[0]);
     }
 
+    public static void main(String[] args){
+//      자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x
+        int n = 12;
+        for(int i=1; i<=n; i++){
+            if(n%i == 1){
+                System.out.println(i);
+                break;
+            }
+        }
+
+    }
+
 
 
 
